5a4a5a145994ffc9d73a61a878a44a33eed060af
jsjahhha981-star <jsjahhha981@gmail.com>
2026-05-02 10:05:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ with st.sidebar:
             padding:10px;
             border-radius:10px;
             margin-bottom:10px;
-            color:black;
+            color:grey;
             font-weight:bold;
         ">
             Dashboard
